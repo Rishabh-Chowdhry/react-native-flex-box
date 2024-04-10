b5bbfbe712a9e6c15fb7f70a728bb3800112f402
Rishabh-Chowdhry <rishabhchodhry.99@gmail.com>
2024-04-11 01:18:56 +0500

    Concept of DeepLinking and Touchable Opacity

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FancyCard from './src/components/FancyCard';
+import ActionCard from './src/components/ActionCard';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <FancyCard />
+        <ActionCard />
       </ScrollView>
     </SafeAreaView>
   );
